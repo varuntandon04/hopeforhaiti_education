c99b8290f4e2ed5cbeff86e903f1a02d67d03dc8
tan <tan@causeverse.io>
2023-06-21 16:22:39 +0900

    implemented the new UI design for payments

--- behaviors/default/shoppingCart.js
+++ behaviors/default/shoppingCart.js
@@ -22,15 +22,15 @@ class ShoppingCartPawn {
       this.popupWindow.style.position = "absolute";
       this.popupWindow.style.top = "20px";
       this.popupWindow.style.left = "20px";
-      this.popupWindow.style.width = "400px";
+      this.popupWindow.style.width = "372px";
       this.popupWindow.style.height = "400px";
       this.popupWindow.style.margin = "0px";
       this.popupWindow.style.zIndex = 1000;
       //this.popupWindow.style.transform = "scale(1.25)"
       this.popupWindow.style.transitionDuration = "0.12s"
-      this.popupWindow.style.borderRadius = "10px"
+      this.popupWindow.style.borderRadius = "25px"
       this.popupWindow.style.boxShadow = "0.15rem 0.3rem 2.5rem rgba(0, 0, 0, 0.5)"
-      this.popupWindow.style.backgroundColor = "rgba(255, 255, 255, 0.4)";
+      this.popupWindow.style.backgroundColor = "rgba(255, 255, 255, 0.85)";
       this.popupWindow.style.backdropFilter = "blur(1.35rem)";
 
       
